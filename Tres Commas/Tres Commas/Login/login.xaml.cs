87bd614266e34aebc240edc4d0269ed361c5f5c2
baby-developer <33032751+baby-developer@users.noreply.github.com>
2019-11-20 21:36:28 +0530

    login window added

---
+++ Tres Commas/Tres Commas/Login/login.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+
+namespace Tres_Commas.Login
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+            SqlConnection con = new SqlConnection("Data Source=(local);Initial Catalog=tres_comma;Integrated Security=SSPI");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT into Registration (FirstName,LastName,Email,Password,Address) values('" + firstname + "','" + lastname + "','" + email + "','" + password + "','" + address + "')", con);
+            cmd.CommandType = CommandType.Text;
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            txtUsername.Text = "";
+            txtPassword.Password = "";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            Close();
+        }
+    }
+}
